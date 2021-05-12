65896228aa412f52ad895f296efccd3410db5ef8
João Tavares <coelhotavares1711@gmail.com>
2021-05-12 14:40:15 -0300

    safari quebra o projeto lol

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 function apresentar(slideAtual) {
-    var slides = document.getElementsByClassName("slide");
+    var slides = document.getElementsByClassName("banner-slide");
     slides[slideAtual - 1].style.display = "none";
     slideAtual = slideAtual % slides.length;
     slides[slideAtual].style.display = "block";
